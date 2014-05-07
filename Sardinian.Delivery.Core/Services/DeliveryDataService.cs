a25aac0dfdab3928d2785330c33408e1d1264584
Richard Moore <richard.koiyan.moore@gmail.com>
2014-05-07 07:11:05 -0400

    Added delivery data service contract.  Need to implement methods.

---
+++ Sardinian.Delivery.Core/Services/DeliveryDataService.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sardinian.Delivery.Core.DTO;
+
+namespace Sardinian.Delivery.Core.Services
+{
+    public class DeliveryDataService : IDeliveryDataService
+    {
+        public Task<CreateLocationResponse> CreateLocation(string accessToken, CreateLocationRequest requestObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreateLocationResponse> UpdateLocation(string accessToken, string locationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreateLocationResponse> DeleteLocation(string accessToken, string locationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreateLocationResponse> GetLocations(string accessToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AddItemResponse> AddItemToCart(string merchantId, AddItemRequest requestObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ModifyItemResponse> ModifyCartItem(string merchantId, ModifyItemRequest requestObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetContentsResponse> GetCartItems(string merchantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AddItemResponse> ClearCart(string merchantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AddItemResponse> RemoveCartItem(string merchantId, string item_key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GenerateGuestToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetPaymentsResponse> GetPayments(string accessToken, string merchantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PlaceOrderResponse> PlaceOrder(string merchantId, PlaceOrderRequest requestObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ViewOrderResponse> GetOrder(string accessToken, string orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ViewOrderHistoryResponse> GetOrderHistory(string accessToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ReorderResponse> Reorder(string accessToken, string orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetCreditCardResponse> GetAvailableCreditCards(string accesToken, bool includeExpiredCards = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetMerchantHoursResponse> GetBusinessAndDeliveryHours(string merchantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetMerchantInfoResponse> GetBusinessInfo(string merchantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetMerchantMenuResponse> GetBusinessMenu(string merchantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetMenuItem(string merchantId, string itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllBusinesses(string method = "delivery")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sardinian.Delivery.Core/Services/IDeliveryDataService.cs
@@ -0,0 +1,54 @@
+﻿using Sardinian.Delivery.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sardinian.Delivery.Core.Services
+{
+    public interface IDeliveryDataService
+    {
+        Task<CreateLocationResponse> CreateLocation(string accessToken, CreateLocationRequest requestObject);
+
+        Task<CreateLocationResponse> UpdateLocation(string accessToken, string locationId);
+
+        Task<CreateLocationResponse> DeleteLocation(string accessToken, string locationId);
+
+        Task<CreateLocationResponse> GetLocations(string accessToken);
+
+        Task<AddItemResponse> AddItemToCart(string merchantId, AddItemRequest requestObject);
+
+        Task<ModifyItemResponse> ModifyCartItem(string merchantId, ModifyItemRequest requestObject);
+
+        Task<GetContentsResponse> GetCartItems(string merchantId);
+
+        Task<AddItemResponse> ClearCart(string merchantId);
+
+        Task<AddItemResponse> RemoveCartItem(string merchantId, string item_key);
+
+        Task<string> GenerateGuestToken();
+
+        Task<GetPaymentsResponse> GetPayments(string accessToken, string merchantId);
+
+        Task<PlaceOrderResponse> PlaceOrder(string merchantId, PlaceOrderRequest requestObject);
+
+        Task<ViewOrderResponse> GetOrder(string accessToken, string orderId);
+
+        Task<ViewOrderHistoryResponse> GetOrderHistory(string accessToken);
+
+        Task<ReorderResponse> Reorder(string accessToken, string orderId);
+
+        Task<GetCreditCardResponse> GetAvailableCreditCards(string accesToken, bool includeExpiredCards = true);
+
+        Task<GetMerchantHoursResponse> GetBusinessAndDeliveryHours(string merchantId);
+
+        Task<GetMerchantInfoResponse> GetBusinessInfo(string merchantId);
+
+        Task<GetMerchantMenuResponse> GetBusinessMenu(string merchantId);
+
+        void GetMenuItem(string merchantId, string itemId);
+
+        void GetAllBusinesses(string method = "delivery");
+    }
+}
